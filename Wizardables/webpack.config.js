85f07988be0eef7abdce195720137914a3ed1381
Niyati Desai <niyatihd@gmail.com>
2019-02-26 11:42:01 -0800

    Complete setup

---
+++ Wizardables/webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/entry_file_wizardables.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env', '@babel/react']
+        }
+      },
+    }]
+  },
+  devtool: 'source-map'
+};
