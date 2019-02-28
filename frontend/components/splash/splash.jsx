331922cef4d6e8d1a89be761cc46c2298db8dc87
Niyati Desai <niyatihd@gmail.com>
2019-02-28 15:16:17 -0800

    Create image carousel for splash component using window.setInterval

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
-// import '../app/assets/stylesheets/api/splash.scss';
 
-const Splash = () => (
-  <div className="splash-bkgrd'">
-    <img id='splash-bkgrd-img' src={window.images.splash_background} />
-  </div>
-);
+class Splash extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentImageIdx: 0
+    };
+
+    this.setImage = this.setImage.bind(this);
+
+    this.images = [
+      window.images.splash1,
+      window.images.splash2,
+      window.images.splash3,
+      window.images.splash4,
+      window.images.splash5,
+      window.images.splash6,
+    ];
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(this.setImage, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+  
+  setImage () {
+    this.setState((state) => {
+      return { currentImageIdx: ((state.currentImageIdx + 1) % 6)};
+    });
+  }
+  
+  render() {
+    return (
+      <div className="splash-bkgrd'">
+        <img id='splash-bkgrd-img' src={this.images[this.state.currentImageIdx]} />
+      </div>
+    )
+  }
+};
 
 export default Splash;
+
+
+
+
