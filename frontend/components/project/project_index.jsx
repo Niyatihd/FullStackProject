7c850744e13f1150895a69b1c37d62c1c2e84845
Niyati Desai <niyatihd@gmail.com>
2019-04-05 10:48:07 -0700

    Fix unique key for each element in project index

--- frontend/components/project/project_index.jsx
+++ frontend/components/project/project_index.jsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import ProjectIndexItem from './project_index_item';
+import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { debug } from 'util';
 
 class ProjectIndex extends React.Component {
   constructor(props) {
     super(props);
-    
-    // this.images = [//icon
-    //   window.images.splash1,
-    //   window.images.lp1,
-    //   window.images.ll3,
-    //   window.images.sk1,
-    //   window.images.w1,
-    //   window.images.al1,
-    //   window.images.f1,
-    //   window.images.am1,
-    //   window.images.vb4,
-    //   window.images.dl2,
-    //   window.images.ob4,
-    //   window.images.st1,
-    //   window.images.tr4,
-    //   window.images.r3,
-    //   window.images.ss6,
-    //   window.images.hh5,
-    //   window.images.rrd4,
-    //   window.images.ng1,
-    //   window.images.qq4,
-    //   window.images.hjp7,
-    // ];
-
+    this.projectOnClick = this.projectOnClick.bind(this);
   }
   
   //refactor into it's own container DELETE
@@ -49,10 +27,16 @@ class ProjectIndex extends React.Component {
     }
   }
 
+  projectOnClick(path) {
+    // debugger
+    this.props.history.push(path);
+  }
+
   render () {
+    // debugger
     let project = this.props.projects.map((project, i) => {
       return (
-        <ProjectIndexItem project={project} key={project.id} followsCount={this.props.followsCount}/>
+        <ProjectIndexItem project={project} key={project.id} followsCount={this.props.followsCount} projectOnClick={this.projectOnClick}/>
       )
     })
 
--- frontend/components/project/project_index_item.jsx
+++ frontend/components/project/project_index_item.jsx
@@ -1,25 +1,24 @@
 // import React from 'react';
-// import { Link } from 'react-router-dom';
 import FollowsContainer from './follows_container';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ProjectIndexItem = props => (
   <li>
-  <div className="project-box">
-    <Link className="project-show" to={`/projects/${props.project.id}`}><img id='project-img' src={props.project.photos[0]} /></Link>
-    <div className="project-info">
-        <img id='project-icon' src={props.project.photos[0]} />
-      <div className="project-content">
-        <h4>{ props.project.title }</h4>
-        {/* <div className="project-follows"> */}
-          {/* <p>{props.project.proj_follows} Followers</p> */}
+    <div className="project-box">
+      <div className="clickable-img" onClick={() => props.projectOnClick(`/projects/${props.project.id}`)}>
+        <img id='project-img' src={props.project.photos[0]} />
+      </div>
+      {/* <Link className="project-show" to={`/projects/${props.project.id}`}><img id='project-img' src={props.project.photos[0]} /></Link> */}
+      <div className="project-info">
+          <img id='project-icon' src={props.project.photos[0]} />
+        <div className="project-content">
+          {/* <h4>{ props.project.title }</h4> */}
+          <Link id="clickable-title" to={`/projects/${props.project.id}`}>{ props.project.title }</Link>
             <FollowsContainer project={props.project} />
-          {/* <Link to="/" className="project-follow-link">Follow</Link> */}
-        {/* </div> */}
+        </div>
       </div>
     </div>
-  </div>
   </li>
 )
 
